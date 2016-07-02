2dc400193861b3e1f9bd3efacfae13861ff39df7
William Almond <walmond@uw.edu>
2016-07-02 00:55:13 +0000

    Initial commit via the website.
    My git folder kept rejecting my work because of files that were not the originals.

---
+++ sort2.c
@@ -0,0 +1,16 @@
+//this is sort2.c
+#include "sort2.h"
+//Quick Sort from Rosetta Code.
+void sort1(int *a, int n) {
+    int h, i, j, t;
+    for (h = n; h /= 2;) {
+        for (i = h; i < n; i++) {
+            t = a[i];
+            for (j = i; j >= h && t < a[j - h]; j -= h) {
+                a[j] = a[j - h];
+            }
+            a[j] = t;
+        }
+    }
+}
+
